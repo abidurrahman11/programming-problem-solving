fef2ee68e78315795c013e674daedb0bda299cbc
abidurrahman11 <abidurr247@gmail.com>
2022-08-17 22:36:45 +0600

    add new problem

---
+++ Elephant.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int x;
+    cin>>x;
+    cout<<(x + 4) / 5;
+    return 0;
+}
+
+//Codeforces Problem: Elephant
+//Status: Accepted
+//Link: https://codeforces.com/problemset/problem/617/A
