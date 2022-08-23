151eb36ded71eeebac4829b438e2ee27bc61a7cc
abidurrahman11 <abidurr247@gmail.com>
2022-08-23 20:19:37 +0600

    add splits

---
+++ Splits.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cin>>n;
+    cout<<(n/2)+1;
+    return 0;
+}
+
+
+//Codeforces Problem: Splits
+//Status: Accepted
+//Link: https://codeforces.com/problemset/problem/964/A
