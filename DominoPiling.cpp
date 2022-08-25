585c8649f4a010e3dd19a91c87673ac8194fbb84
abidurrahman11 <abidurr247@gmail.com>
2022-08-25 12:19:08 +0600

    new pb

---
+++ DominoPiling.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int m, n;
+    cin>>m>>n;
+    int ans = (m*n)/2;
+    cout<<ans;
+    return 0;
+}
+
+
+//Codeforces Problem: Domino piling
+//Status: Accepted
+//Link: https://codeforces.com/problemset/problem/50/A
