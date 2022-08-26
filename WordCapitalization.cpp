2e6b0ec2b6afe09a113c11186f36853f41a40e4c
abidurrahman11 <abidurr247@gmail.com>
2022-08-26 14:05:13 +0600

    word

---
+++ WordCapitalization.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    string s;
+    cin>>s;
+    s[0] = towupper(s[0]);
+    cout<<s;
+    return 0;
+}
+
+
+//Codeforces Problem: Word Capitalization
+//Status: Accepted
+//Link: https://codeforces.com/problemset/problem/281/A
