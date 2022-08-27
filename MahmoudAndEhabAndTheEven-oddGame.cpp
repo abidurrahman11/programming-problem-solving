83070ff9aca3a321adcc6a0a113e5b05de711d38
abidurrahman11 <abidurr247@gmail.com>
2022-08-27 18:57:35 +0600

    add 1

---
+++ MahmoudAndEhabAndTheEven-oddGame.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cin>>n;
+    if (n%2 == 0) {
+        cout<<"Mahmoud";
+    } else {
+        cout<<"Ehab";
+    }
+    return 0;
+}
+
+
+//Codeforces Problem: Mahmoud and Ehab and the even-odd game
+//Status: Accepted
+//Link: https://codeforces.com/problemset/problem/959/A
