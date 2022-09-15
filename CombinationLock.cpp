104dc3ab07c5097b8fe268e14d4d3d8d8dcecdc6
abidurrahman11 <abidurr247@gmail.com>
2022-09-15 11:27:44 +0600

    boring

---
+++ CombinationLock.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+
+using namespace std;
+
+int main () {
+    int n, moves = 0;
+    string s1, s2;
+    cin>>n>>s1>>s2;
+    for (int i = 0; i < n; i++) {
+        int d1 = abs(s1[i] - s2[i]);
+        int d2 = 10 - d1;
+        moves += min(d1, d2);
+    }
+    cout<<moves;
+    return 0;
+}
+
+
+//Codeforces Problem: Combination Lock
+//Status: Accepted
+//Link: https://codeforces.com/problemset/problem/540/A
