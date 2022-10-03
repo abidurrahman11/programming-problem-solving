89cdca674f1ca00af5f125bf6986dca63d00ccd5
abidurrahman11 <abidurr247@gmail.com>
2022-10-03 22:24:54 +0600

    contest

---
+++ WorkingWeek.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void ans () {
+    long long n;
+    cin>>n;
+    cout<< (n / 3) - 2<<"\n";
+}
+
+int main () {
+    int t;
+    cin>>t;
+    while (t--) {
+        ans();
+    }
+    return 0;
+}
+
+
+//Codeforces Problem: Working Week
+//Status: Accepted
+//Link: https://codeforces.com/contest/1735/problem/A
