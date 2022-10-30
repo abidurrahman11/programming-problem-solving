3d6b559a8167c82fe7461087994b1548297a97e8
abidurrahman11 <abidurr247@gmail.com>
2022-10-30 19:28:15 +0600

    implementation

---
+++ ICPCBalloons.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while (t--) {
+        int n;
+        cin>>n;
+        set <char> st;
+        for (int i = 0; i < n; i++) {
+            char c;
+            cin>>c;
+            st.insert(c);
+        }
+        cout<<(st.size() * 2) + (n - st.size())<<"\n";
+    }
+        
+    return 0;
+}
