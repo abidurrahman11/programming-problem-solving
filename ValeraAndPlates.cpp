78d3f8a64f6dbb40523eb43d52052f69326b0b26
abidurrahman11 <abidurr247@gmail.com>
2022-11-10 23:57:29 +0600

    array

---
+++ ValeraAndPlates.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main () {
+    int n, m, k, ans = 0;
+    cin>>n>>m>>k;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin>>arr[i];
+    }
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == 1) {
+            if (m > 0) {
+                m--;
+            } else if (m == 0) {
+                ans++;
+            }
+        } else if (arr[i] == 2) {
+            if (k > 0) {
+                k--;
+            } else if (m > 0) {
+                m--;
+            } else {
+                ans++;
+            }
+        }
+    }
+    cout<<ans;
+    return 0;
+}
