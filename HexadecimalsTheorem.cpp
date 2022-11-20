cfbd5c042c7afab3bf797f7140511cdaeaf91a3a
abidurrahman11 <abidurr247@gmail.com>
2022-11-20 23:43:20 +0600

    operators

---
+++ HexadecimalsTheorem.cpp
@@ -0,0 +1,13 @@
+// Codeforces Problem: Hexadecimal's theorem
+// Link: https://codeforces.com/problemset/problem/199/A
+// Status: Accepted      Language: C++
+
+#include<iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cin>>n;
+    cout<<"0 0 "<<n;
+    return 0;
+}
