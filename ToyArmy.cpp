2d7d87564778cae6c7d5618878c05e6385ba686d
abidurrahman11 <abidurr247@gmail.com>
2022-11-25 22:02:09 +0600

    math

---
+++ ToyArmy.cpp
@@ -0,0 +1,13 @@
+// Codeforces Problem: Toy Army
+// Link: https://codeforces.com/problemset/problem/84/A
+// Status: Accepted      Language: C++
+
+#include<iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cin>>n;
+    cout<<(n + n) - (n / 2);
+    return 0;
+}
