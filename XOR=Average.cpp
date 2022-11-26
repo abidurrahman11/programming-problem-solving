013474e021d79ae3abd7758680dee21d014d8b0f
abidurrahman11 <abidurr247@gmail.com>
2022-11-26 23:29:37 +0600

    string

---
+++ XOR=Average.cpp
@@ -0,0 +1,27 @@
+// Codeforces Problem: 
+// Link: 
+// Status: Accepted      Language: C++
+
+#include<iostream>
+using namespace std;
+
+int main () {
+    int t;
+    cin>>t;
+    while (t--) {
+        int n;
+        cin>>n;
+        if (n%2 == 1) {
+            for (int i = 0; i < n; i++) {
+                cout<<7<<" ";
+            }
+        } else {
+            cout<<"1 3 ";
+            for (int i = 0; i < n - 2; i++) {
+                cout<<"2 ";
+            }
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
