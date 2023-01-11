3e7201311fa12aa94c3067e39b681dc08d7cf370
abidurrahman11 <abidurr247@gmail.com>
2023-01-11 23:37:43 +0600

    loop

---
+++ LittleElephantAndFunction.cpp
@@ -0,0 +1,16 @@
+// Codeforces Problem: 221A - Little Elephant and Function
+// Link: https://codeforces.com/problemset/problem/221/A
+// Status: Accepted      Language: C++
+
+#include<iostream>
+using namespace std;
+
+int main () {
+    int n;
+    cin>>n;
+    cout<<n<<" ";
+    for (int i = 1; i < n; i++) {
+        cout<<i<<" ";
+    }
+    return 0;
+}
