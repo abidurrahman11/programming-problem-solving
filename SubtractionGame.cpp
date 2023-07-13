08234b16fce9ee345c7f825aa0a505022ad6dd3e
abidurrahman11 <abidurr247@gmail.com>
2023-07-13 18:38:09 +0600

    game

---
+++ SubtractionGame.cpp
@@ -0,0 +1,28 @@
+// Codeforces Problem: A - Subtraction Game
+// Link: https://codeforces.com/contest/1844/problem/A
+// Status: Accepted      Language: C++
+
+#include<iostream>
+// #include<bits/stdc++.h>
+// #include<set>
+// #include<vector>
+// #include<map>
+// #include<algorithm>
+using namespace std;
+
+#define ll long long
+#define nl "\n"
+#define ws " "
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define MOD 1000000007
+
+int main () {
+    int t;
+    cin>>t;
+    while (t--) {
+        int a, b;
+        cin>>a>>b;
+        cout<<a + b<<nl;
+    }
+    return 0;
+}
