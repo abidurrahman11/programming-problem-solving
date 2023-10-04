66109b3de4f2526423e5ac0809070414c24a49d7
abidurrahman11 <abidurr247@gmail.com>
2023-10-04 22:52:00 +0600

    d 3

---
+++ Aleksa and Stack.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+// #include<bits/stdc++.h>
+// #include<set>
+// #include<vector>
+// #include<map>
+// #include<algorithm>
+using namespace std;
+
+#define ll long long
+#define nl "\n"
+#define ws " "
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define MOD 1000000007
+
+int main () {
+    int t;
+    cin>>t;
+    while (t--) {
+        int n;
+        cin>>n;
+        int ini = 1;
+        for (int i = 0; i < n; i++) {
+            cout<<ini<<ws;
+            ini += 2;
+        }
+        cout<<nl;
+    }
+    return 0;
+}
